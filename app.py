c6951b15983024eaebab39e9a0b052e7a83cda77
Warren Crasta <warrencrasta@gmail.com>
2017-01-03 14:37:53 -0600

    Cleanup

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 from operator import itemgetter
 from flask import Flask, render_template, request, redirect, url_for
-from app2 import app2_object
 from urllib.parse import parse_qs, urlparse
 
+# Create the Flask application.
 app = Flask(__name__)
-app.register_blueprint(app2_object)
+# app.debug = True
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -29,11 +29,10 @@ def tools():
         seasonId = request.args.get('seasonId')
         url = 'http://games.espn.com/fba/standings?leagueId={}&seasonId={}'.format(leagueId, seasonId)
     try:
-        teams, categories = setup(url)
+        setup(url)
     except:
         return redirect(url_for('index', invalidURL=True))
-    season_rankings, season_matchups, season_analysis = computeStats(teams, categories)
-    return render_template('tools.html', season_rankings=season_rankings, season_matchups=season_matchups, season_analysis=season_analysis, leagueId=leagueId, seasonId=seasonId)
+    return render_template('tools.html', leagueId=leagueId, seasonId=seasonId)
 
 @app.route('/season_rankings')
 def season_rankings():
@@ -41,10 +40,10 @@ def season_rankings():
     seasonId = request.args.get('seasonId')
     url = 'http://games.espn.com/fba/standings?leagueId={}&seasonId={}'.format(leagueId, seasonId)
     try:
-        teams, categories = setup(url)
+        teams, categories, seasonData = setup(url)
     except:
         return redirect(url_for('index', invalidURL=True))
-    season_rankings, season_matchups, season_analysis = computeStats(teams, categories)
+    season_rankings, season_matchups, season_analysis = computeStats(teams, categories, seasonData)
     return render_template('season_rankings.html', season_rankings=season_rankings, leagueId=leagueId, seasonId=seasonId)
 
 @app.route('/season_matchups')
@@ -53,10 +52,10 @@ def season_matchups():
     seasonId = request.args.get('seasonId')
     url = 'http://games.espn.com/fba/standings?leagueId={}&seasonId={}'.format(leagueId, seasonId)
     try:
-        teams, categories = setup(url)
+        teams, categories, seasonData = setup(url)
     except:
         return redirect(url_for('index', invalidURL=True))
-    season_rankings, season_matchups, season_analysis = computeStats(teams, categories)
+    season_rankings, season_matchups, season_analysis = computeStats(teams, categories, seasonData)
     return render_template('season_matchups.html', season_matchups=season_matchups, leagueId=leagueId, seasonId=seasonId)
 
 @app.route('/season_analysis')
@@ -65,66 +64,136 @@ def season_analysis():
     seasonId = request.args.get('seasonId')
     url = 'http://games.espn.com/fba/standings?leagueId={}&seasonId={}'.format(leagueId, seasonId)
     try:
-        teams, categories = setup(url)
+        teams, categories, seasonData = setup(url)
     except:
         return redirect(url_for('index', invalidURL=True))
-    season_rankings, season_matchups, season_analysis = computeStats(teams, categories)
-    return render_template('season_analysis.html', season_matchups=season_matchups, season_analysis=season_analysis, leagueId=leagueId, seasonId=seasonId)
+    season_rankings, season_matchups, season_analysis = computeStats(teams, categories, seasonData)
+    return render_template('season_analysis.html', season_matchups=season_matchups, season_analysis=season_analysis,
+                           leagueId=leagueId, seasonId=seasonId)
+
+@app.route('/weekly_rankings')
+def weekly_rankings():
+    leagueId = request.args.get('leagueId')
+    seasonId = request.args.get('seasonId')
+    url = 'http://games.espn.com/fba/scoreboard?leagueId={}&seasonId={}'.format(leagueId, seasonId)
+    try:
+        teams, categories, seasonData = setup(url)
+    except:
+        return redirect(url_for('index', invalidURL=True))
+    weekly_rankings, weekly_matchups, weekly_analysis = computeStats(teams, categories, seasonData)
+    return render_template('weekly_rankings.html', weekly_rankings=weekly_rankings, leagueId=leagueId, seasonId=seasonId)
+
+@app.route('/weekly_matchups')
+def weekly_matchups():
+    leagueId = request.args.get('leagueId')
+    seasonId = request.args.get('seasonId')
+    url = 'http://games.espn.com/fba/scoreboard?leagueId={}&seasonId={}'.format(leagueId, seasonId)
+    try:
+        teams, categories, seasonData = setup(url)
+    except:
+        return redirect(url_for('index', invalidURL=True))
+    weekly_rankings, weekly_matchups, weekly_analysis = computeStats(teams, categories, seasonData)
+    return render_template('weekly_matchups.html', weekly_matchups=weekly_matchups, leagueId=leagueId, seasonId=seasonId)
+
+@app.route('/weekly_analysis')
+def weekly_analysis():
+    leagueId = request.args.get('leagueId')
+    seasonId = request.args.get('seasonId')
+    url = 'http://games.espn.com/fba/scoreboard?leagueId={}&seasonId={}'.format(leagueId, seasonId)
+    try:
+        teams, categories, seasonData = setup(url)
+    except:
+        return redirect(url_for('index', invalidURL=True))
+    weekly_rankings, weekly_matchups, weekly_analysis = computeStats(teams, categories, seasonData)
+    return render_template('weekly_analysis.html', weekly_matchups=weekly_matchups, weekly_analysis=weekly_analysis,
+                           leagueId=leagueId, seasonId=seasonId)
 
 # Scrapes the "Season Stats" table from the ESPN Fantasy Standings page.
 def setup(url):
     source_code = requests.get(url)
     plain_text = source_code.text
-    soup = BeautifulSoup(plain_text, 'html.parser')
-
-    seasonStats = soup.find('table', {'id': 'statsTable'})
+    soup = BeautifulSoup(plain_text, 'lxml')
     teams = []
-    categories = [link.string for link in seasonStats.findAll('tr')[2].findAll('a')]
-  
-    rows = seasonStats.findAll('tr')[3:]
+    # Season standings have a different URL than weekly scoreboard
+    seasonData = url.startswith('http://games.espn.com/fba/standings')
+    # Scrape table depending on whether it's season or weekly data.
+    if seasonData:
+        seasonStats = soup.find('table', {'id': 'statsTable'})
+        categories = [link.string for link in seasonStats.findAll('tr')[2].findAll('a')]
+        rows = seasonStats.findAll('tr')[3:]
+    else:
+        tableSubHead = soup.find_all('tr', class_='tableSubHead')
+        tableSubHead = tableSubHead[0]
+        listCats = tableSubHead.find_all('th')
+        categories = []
+        for cat in listCats:
+            if 'title' in cat.attrs:
+                categories.append(cat.string)
+        rows = soup.findAll('tr', {'class': 'linescoreTeamRow'})
+
     # Creates a 2-D matrix which resembles the Season Stats table.
     for row in range(len(rows)):
         team_row = []
-        # The first 3 columns are always present.
-        for column in rows[row].findAll('td')[:(3+len(categories))]:
+        # Season Data values always have 3 extra columns, weekly data always has 2 extra columns when scraping.
+        if seasonData:
+            columns = rows[row].findAll('td')[:(3 + len(categories))]
+        else:
+            columns = rows[row].findAll('td')[:(2 + len(categories))]
+        for column in columns:
             team_row.append(column.getText())
-
         # Add each team to a teams matrix.
         teams.append(team_row)
-    return teams, categories
+    return teams, categories, seasonData
 
-# Computes the power rankings and matchup predictions, and stores the
-# values in a tuple.
-def computeStats(teams, categories):
-    # Initialize the dictionary which will hold information about each team
-    # along with their "power rankings score".
+# Computes the standings and matchups.
+def computeStats(teams, categories, seasonData):
+    # Initialize the dictionary which will hold information about each team along with their "standings score".
     teamDict = {}
     for team in teams:
-        teamDict[team[1]] = 0
+        if seasonData:
+            teamDict[team[1]] = 0
+        else:
+            teamDict[team[0]] = 0
 
     matchupsList = []
     analysisList = []
     for team1 in teams:
         for team2 in teams:
-            score, wonList, lossList = calculateScore(team1[3:], team2[3:], categories)
+            if seasonData:
+                score, wonList, lossList, tiesList = calculateScore(team1[3:], team2[3:], categories)
+            else:
+                score, wonList, lossList, tiesList = calculateScore(team1[2:], team2[2:], categories)
             if team1 != team2:
                 # The value for the dictionary is the power rankings score. A win increases the score and a loss
                 # decreases the "PR" score.
                 if score[0] > score[1]:
-                    teamDict[team1[1]] += 1
+                    if seasonData:
+                        teamDict[team1[1]] += 1
+                    else:
+                        teamDict[team1[0]] += 1
                 elif score[0] < score[1]:
-                    teamDict[team1[1]] -= 1
-                # map(str, score) is for formatting the score tuple into a
-                # string.
-                matchupsList.append(
-                    team1[1] + ' vs. ' + team2[1] + ' || SCORE (W-L-T): ' + '-'.join(map(str, score)))
-                analysisList.append(team1[1] + ' vs. ' + team2[1] + ' -- ' + team1[1] + ' won ' + ', '.join(wonList) + '. '
-                + team1[1] + ' lost ' + ', '.join(lossList) + '.')
+                    if seasonData:
+                        teamDict[team1[1]] -= 1
+                    else:
+                        teamDict[team1[0]] -= 1
+                # map(str, score) is for formatting the score tuple into a string.
+                if seasonData:
+                    matchupsList.append(
+                        team1[1] + ' vs. ' + team2[1] + ' || SCORE (W-L-T): ' + '-'.join(map(str, score)))
+                    analysisList.append(team1[1] + ' vs. ' + team2[1] + ' -- ' + team1[1] + ' won ' + ', '.join(wonList) + '. '
+                    + team1[1] + ' lost ' + ', '.join(lossList) + '. ' + team1[1] +' tied ' + ', '.join(tiesList) + '.')
+                else:
+                    matchupsList.append(
+                        team1[0] + ' vs. ' + team2[0] + ' || SCORE (W-L-T): ' + '-'.join(map(str, score)))
+                    analysisList.append(
+                        team1[0] + ' vs. ' + team2[0] + ' -- ' + team1[0] + ' won ' + ', '.join(wonList) + '. '
+                        + team1[0] + ' lost ' + ', '.join(lossList) + '. ' + team1[0] + ' tied ' + ', '.join(
+                            tiesList) + '.')
         matchupsList.append('*' * 100)
         analysisList.append('*' * 100)
 
     # Check if two keys in the dictionary have the same value (used to process
-    # ties in PR score).
+    # ties in standings score).
     result = {}
     for val in teamDict:
         if teamDict[val] in result:
@@ -132,13 +201,13 @@ def computeStats(teams, categories):
         else:
             result[teamDict[val]] = [val]
 
-    # Sort the dictionary by greatest PR score.
+    # Sort the dictionary by greatest standings score.
     sortedDict = sorted(result.items(), key=itemgetter(0), reverse=True)
 
-    # Contains the Power Rankings.
+    # Contains the standings.
     rankingsList = []
     counter = 1
-    # Keys are the PR score, values are the team names.
+    # Keys are the standings score, values are the team names.
     for k, v in sortedDict:
         rankingsList.append(str(counter) + '. ' + ', '.join(v))
         counter += 1
@@ -152,6 +221,7 @@ def calculateScore(teamA, teamB, categories):
     ties = 0
     wonList = []
     lossList = []
+    tiesList = []
 
     turnoverCol = -1
     for category in categories:
@@ -171,6 +241,7 @@ def calculateScore(teamA, teamB, categories):
                     wonList.append(categories[i] + ' (' + str(b-a) + ')')
                 elif a == b:
                     ties += 1
+                    tiesList.append(categories[i] + ' (' + str(b-a) + ')')
                 else:
                     losses += 1
                     lossList.append(categories[i] + ' (' + str(b-a) + ')')
@@ -180,11 +251,12 @@ def calculateScore(teamA, teamB, categories):
                     wonList.append(categories[i] + ' (' + str(round((a-b), 4)) +')')
                 elif a == b:
                     ties += 1
+                    tiesList.append(categories[i] + ' (' + str(round((a - b), 4)) + ')')
                 else:
                     losses += 1
                     lossList.append(categories[i] + ' (' + str(round((a-b), 4)) + ')')
 
-    valuesTuple = ((wins, losses, ties), wonList, lossList)
+    valuesTuple = ((wins, losses, ties), wonList, lossList, tiesList)
     return valuesTuple
 
 # Run the Flask app.
@@ -192,5 +264,5 @@ if __name__ == '__main__':
     app.run()
 
 # Comment out the if statement above and uncomment the line below to debug Python code.
-# teams, categories = setup('http://games.espn.com/fba/standings?leagueId=224165&seasonId=2017')
-# matchups, rankings, list2 = computeStats(teams, categories)
+# teams, categories, seasonData = setup('http://games.espn.com/fba/standings?leagueId=224165&seasonId=2017')
+# rankingsList, matchupsList, analysisList = computeStats(teams, categories, seasonData)
